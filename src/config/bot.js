535ee6e269f1657b275450f7ac6ebf60b2d777a7
keselmandanny-droid <keselman.danny@gmail.com>
2026-05-02 13:41:54 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Legacy-Bot",
+        name: "Created by Link25",
         // Activity type number (0 = Playing).
         type: 1, 
       },
